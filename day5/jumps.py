979508648652cb787d15e31c35f20c8e31a495c3
Alex Petrescu <alex.petrescu@gmail.com>
2017-12-09 20:52:23 -0800

    day5
    jumps part 1 and part 2

---
+++ day5/jumps.py
@@ -0,0 +1,13 @@
+input_file = open("input.txt", 'r')
+instructions = input_file.readlines()
+jumps = 0
+idx = 0
+print(instructions)
+while idx < len(instructions):
+    instruction = int(instructions[idx])
+    instructions[idx] = instruction + 1
+    idx = idx + instruction
+    jumps += 1
+    #print(instructions)
+
+print(jumps)
